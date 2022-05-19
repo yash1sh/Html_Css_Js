7a1240c54fbb81a33fec8420d6d912d697012e94
Yash Shetty <yashshetty@gmail.com>
2022-05-19 14:45:05 +0530

    edited this.js

--- this.js
+++ this.js
@@ -1,12 +1,16 @@
 "use strict";
 
+var randomname = "priyash";
+
 const r = {
   name: "yash",
   age: 20,
   printdel: function () {
     console.log(`Name is ${this.name} and Age is ${this.age}`);
   },
-  ageafter: () => {
-    console.log(`Age after 20 is ${this.age * 20}`);
+  ran: () => {
+    console.log(`Random name taken as  ${this.randomname}`);
   },
+  //In arrow funtions this doesnt has nay value so its a random funtion Here the ans is priyash because var keeps these property in windows hence in arrow funtion this.random name is priyash
+  
 };
